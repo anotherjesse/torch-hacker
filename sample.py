2359f8161a3629006b1724722c78e80245661685
Jesse Andrews <anotherjesse@gmail.com>
2024-01-28 17:29:58 +0000

    support running examples local and remote

--- sample.py
+++ sample.py
@@ -4,7 +4,7 @@ import sys
 import yaml
 from pathlib import Path
 import glob
-
+import replicate
 
 def run(**kwargs):
     r = requests.post("http://localhost:5000/predictions", json={"input": kwargs})
@@ -12,13 +12,13 @@ def run(**kwargs):
     rv = r.json()
 
     if rv["status"] == "succeeded":
-        print("output:", rv["output"])
+        return "output: %s" % rv["output"]
 
     if rv["error"]:
-        print("Error:", rv["error"])
+        return "Error: %s" % rv["error"]
 
 
-def run_model(model_dir):
+def run_model(model_dir, r8_model_and_version=None):
     model_dir = Path(model_dir)
     cog = yaml.load(open(model_dir / "cog.yaml"), Loader=yaml.Loader)
     print("cog:", cog)
@@ -40,13 +40,26 @@ def run_model(model_dir):
         # we parse and re-serialize to ensure that we are sending valid json
         inputs = json.dumps(inputs)
         print("inputs:", inputs)
-        output = run(inputs=inputs, code=code, pip=pip, apt=apt)
+        if r8_model_and_version is None:
+            output = run(inputs=inputs, code=code, pip=pip, apt=apt)
+        else:
+            output = replicate.run(r8_model_and_version, input={
+                "inputs": inputs,
+                "code": code,
+                "pip": pip,
+                "apt": apt
+            })
+        print(output)
+
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("usage: python sample.py model_dir")
+    if len(sys.argv) == 2:
+        run_model(sys.argv[1])
+    elif len(sys.argv) == 3:
+        run_model(sys.argv[2], r8_model_and_version=sys.argv[1])
+    else:
+        print("usage: python sample.py (optional:r8_model_version) model_dir")
         sys.exit(1)
 
-    run_model(sys.argv[1])
 # run(pip="transformers diffusers accelerate", inputs=json.dumps({"prompt": "whippet"}))
